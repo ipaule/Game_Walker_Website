65a5e4525b88538d4294fd2cf63f465c4afec0a8
ipaule <ipaule@berkeley.edu>
2024-10-17 02:28:41 -0700

    implemented LaunchScreen, initial main screen, attatched link to Download

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
+import LaunchScreen from './LaunchScreen';
 
 function App() {
+  const [showLaunchScreen, setShowLaunchScreen] = useState(true);
+  const [showNotification, setShowNotification] = useState(false);
+
+  const handleLaunchScreenFinish = useCallback(() => {
+    setShowLaunchScreen(false);
+  }, []);
+
+  useEffect(() => {
+    if (!showLaunchScreen) {
+      document.body.classList.add('main-screen');
+    }
+  }, [showLaunchScreen]);
+
+  const handleDownloadClick = (e) => {
+    e.preventDefault();
+    const appStoreUrl = 'https://apps.apple.com/us/app/game-walker/id6476948969?l=ko';
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    const isAndroid = /Android/.test(navigator.userAgent);
+
+    if (isIOS) {
+      window.location.href = appStoreUrl;
+    } else if (isAndroid) {
+      setShowNotification(true);
+      setTimeout(() => setShowNotification(false), 5000); // Hide notification after 5 seconds
+    } else {
+      // For desktop computers, redirect to the App Store page
+      window.open(appStoreUrl, '_blank');
+    }
+  };
+
+  if (showLaunchScreen) {
+    return <LaunchScreen onFinish={handleLaunchScreenFinish} />;
+  }
+
   return (
-    <div className="App">
+    <div className="App main-screen">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={process.env.PUBLIC_URL + '/png/GameWalkerLogo.png'} className="App-logo" alt="Game Walker logo" />
+        <nav className="App-nav">
+          <ul>
+            <li><a href="#" onClick={handleDownloadClick}>Download</a></li>
+            <li><a href="#setup">Setup</a></li>
+            <li><a href="#features">Features</a></li>
+            <li><a href="#people">People</a></li>
+          </ul>
+        </nav>
       </header>
+      {showNotification && (
+        <div className="notification">
+          We apologize, but Game Walker is currently only available for iOS devices. We appreciate your interest and hope to support more platforms in the future!
+        </div>
+      )}
     </div>
   );
 }
